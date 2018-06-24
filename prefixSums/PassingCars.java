dd4cb6c37ff774e61bc899c066b068b9da37e130
Hilal Tarakci <hlltarakci@gmail.com>
2018-06-24 19:31:28 +0300

    [add] solution for PassingCars

---
+++ prefixSums/PassingCars.java
@@ -0,0 +1,30 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // 0: east -->
+        // 1: west <--
+        // p: -->
+        // q:   <--   passing
+        // for each left 0, there are as many passing cars as 1 s on its right
+        // in other words,
+        // ... for each 1, there are as many passing cars as 0 s on its left
+    
+        int len = A.length;
+        int eastCount = 0;
+        int passingCount = 0;
+        
+        for(int val: A) {
+            if(val == 0) { eastCount++; }
+            else { passingCount += eastCount; }
+            
+            if(passingCount > 1000000000) return -1;
+        }
+        
+        return passingCount;
+    }
+}
